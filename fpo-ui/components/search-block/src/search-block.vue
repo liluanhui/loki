c0320bc0a02aeae42023e5c28ab199bc470b38f8
Sam <llh@birdpaper.com>
2025-04-17 06:13:04 +0800

    feat: 更新 SearchBlock 组件样式，添加聚焦效果，修复样式问题；更新 BannerBlock 背景颜色

--- fpo-ui/components/search-block/src/search-block.vue
+++ fpo-ui/components/search-block/src/search-block.vue
@@ -1,9 +1,14 @@
 <template>
-  <div :class="clsBlockName">
+  <div :class="[clsBlockName, isFocus && `${clsBlockName}-focus`]">
     <IconSearch2Line size="23" />
+
     <div :class="`${clsBlockName}-input`">
-      <input v-model="text" type="text" placeholder="Search invoice number" />
-      <!-- <input v-model="text" type="text" placeholder="查询信件｜单号｜邮箱地址" /> -->
+      <!-- <input v-model="text" type="text" placeholder="Search invoice number" /> -->
+      <input v-model="text" type="text" placeholder="查询信件｜单号｜邮箱地址" @focus="isFocus = true" @blur="isFocus = false" />
+    </div>
+
+    <div :class="`${clsBlockName}-tip`">
+      
     </div>
   </div>
 </template>
@@ -16,4 +21,5 @@ const clsBlockName = "search-block";
 
 const text = ref("");
 // const text = ref("SF12398789327509423");
+const isFocus = ref(false);
 </script>
