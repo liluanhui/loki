84ef9701d1781e8145a5f0728ae140ca3e41695a
Sam <llh@birdpaper.com>
2025-04-17 15:31:24 +0800

    feat: 添加 TitleBar 组件，更新首页布局，调整样式；优化 BannerBlock 和 ShortcutKey 组件样式

--- app/src/views/home/index.vue
+++ app/src/views/home/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div :class="clsBlockName">
-    <banner-block></banner-block>
+    <banner-block />
+    <search-block />
 
-    <search-block></search-block>
+    <div class="quick-start">
+      <bp-button :icon="IconPenNibFill" size="large" status="warning" type="dashed" shape="round">写一封信</bp-button>
+    </div>
+
+    <title-bar title="公开信" />
+    <div style="width: 1200px; height: 300px; background-color: #fff; border-radius: 16px"></div>
   </div>
 </template>
 
 <script setup lang="ts">
+import titleBar from "./components/title-bar.vue";
+import { IconPenNibFill } from "birdpaper-icon";
+
 defineOptions({ name: "HomePage" });
 const clsBlockName = "home-page";
 </script>
