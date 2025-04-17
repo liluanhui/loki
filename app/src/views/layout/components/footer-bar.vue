5ddc6fc3078a9c6e273315279474a734cc8e6b2a
Sam <llh@birdpaper.com>
2025-04-17 21:55:33 +0800

    feat: 更新 footer-bar 组件样式，调整布局；修复 footer-bar 高度设置，添加统计信息展示

--- app/src/views/layout/components/footer-bar.vue
+++ app/src/views/layout/components/footer-bar.vue
@@ -1,12 +1,29 @@
 <template>
   <footer :class="clsBlockName">
     <div :class="`${clsBlockName}-inner`">
-      footer
+      <div class="first"></div>
+      <div class="second">
+        未来邮局已开业 <span>206</span> 天
+        <bp-statistic v-model="a" show-separator unit="封">
+          <template #prefix>已投递</template>
+        </bp-statistic>
+        <bp-statistic v-model="b" show-separator unit="封">
+          <template #prefix>待投递</template>
+        </bp-statistic>
+      </div>
     </div>
   </footer>
+  <div :class="`${clsBlockName}-number`">
+    <bp-link status="gray" size="mini" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备17015354号</bp-link>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
+
 defineOptions({ name: "FooterBar" });
 const clsBlockName = "footer-bar";
+
+const a = ref(10244);
+const b = ref(322);
 </script>
