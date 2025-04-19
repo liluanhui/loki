7e1a81eedcfdf02f39068c059820797714788416
Sam <llh@birdpaper.com>
2025-04-19 15:12:58 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   app/src/scss/layout/footer-bar.scss #	modified:   app/src/scss/layout/top-bar.scss #	modified:   fpo-ui/components/lang-trigger/src/lang-trigger.vue #	modified:   fpo-ui/components/theme-trigger/src/theme-trigger.vue #	modified:   fpo-ui/style/common/extend.scss

--- fpo-ui/components/lang-trigger/src/lang-trigger.vue
+++ fpo-ui/components/lang-trigger/src/lang-trigger.vue
@@ -1,10 +1,25 @@
 <template>
   <div :class="clsBlockName">
-    <IconTranslate2 size="16" />
+    <bp-dropdown @select="handleSelect">
+      <IconTranslate2 size="16" />
+
+      <template #content>
+        <bp-doption v-for="v in langList" :value="v.value">{{ v.title }}</bp-doption>
+      </template>
+    </bp-dropdown>
   </div>
 </template>
 
 <script setup lang="ts">
 defineOptions({ name: "LangTrigger" });
 const clsBlockName = "lang-trigger";
+
+const langList = [
+  { title: "中文 - 简", value: "zh-CN" },
+  { title: "English", value: "en" },
+];
+
+const handleSelect = (value: string) => {
+  console.log("Selected value:", value);
+};
 </script>
--- fpo-ui/components/theme-trigger/src/theme-trigger.vue
+++ fpo-ui/components/theme-trigger/src/theme-trigger.vue
@@ -1,10 +1,41 @@
 <template>
-  <div :class="clsBlockName">
-    <IconSunLine size="16" />
+  <div :class="clsBlockName" @click="handleTrigger">
+    <Transition name="fade-page" mode="out-in">
+      <IconSunLine v-if="currentTheme === 'light'" size="16" />
+      <IconMoonLine v-else size="16" />
+    </Transition>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useStorage } from "@vueuse/core";
+import { watch } from "vue";
+
 defineOptions({ name: "ThemeTrigger" });
 const clsBlockName = "theme-trigger";
+
+const currentTheme = useStorage("theme", "light");
+const handleTrigger = () => {
+  currentTheme.value = currentTheme.value === "light" ? "dark" : "light";
+};
+
+watch(
+  () => currentTheme.value,
+  (newTheme) => {
+    document.documentElement.setAttribute("class", newTheme);
+  },
+  { immediate: true }
+);
 </script>
+
+<style lang="scss">
+.fade-page-enter-active,
+.fade-page-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-page-enter-from,
+.fade-page-leave-to {
+  opacity: 0;
+}
+</style>
