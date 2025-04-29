1dd01c2c8ee7c092421fc90a87b736f76f397525
Sam <llh@birdpaper.com>
2025-04-29 10:41:05 +0800

    feat: 调整表单样式，优化编辑器布局，更新标签宽度和内边距

--- app/src/views/write/index.vue
+++ app/src/views/write/index.vue
@@ -7,41 +7,56 @@
 
       <div :class="`${clsBlockName}-editor`">
         <div :class="`${clsBlockName}-form`">
-          <div :class="`${clsBlockName}-form-item`">
-            <label :class="`${clsBlockName}-form-item-label`">寄给</label>
-            <div :class="`${clsBlockName}-form-item-content`">
-              <radio-bar v-model="current" size="small" :option-list="radioBarList"></radio-bar>
-            </div>
-          </div>
-          <div v-if="current === 'email'" :class="`${clsBlockName}-form-item`">
-            <label :class="`${clsBlockName}-form-item-label`">收件人</label>
-            <div :class="`${clsBlockName}-form-item-content`">
-              <bp-input placeholder="输入邮件地址" :style="{ width: '260px' }" clearable> </bp-input>
-            </div>
-          </div>
-          <div v-if="current === 'email'" :class="`${clsBlockName}-form-item`">
-            <label :class="`${clsBlockName}-form-item-label`">收件人称呼</label>
-            <div :class="`${clsBlockName}-form-item-content`">
-              <bp-input placeholder="输入对方的称呼" :style="{ width: '260px' }" clearable> </bp-input>
-            </div>
-          </div>
-          <div :class="`${clsBlockName}-form-item`">
-            <label :class="`${clsBlockName}-form-item-label`">主题</label>
-            <div :class="`${clsBlockName}-form-item-content`">
-              <bp-input placeholder="信件主题" :style="{ width: '560px' }" clearable :maxlength="25" show-limit> </bp-input>
-            </div>
-          </div>
-          <div :class="`${clsBlockName}-form-item`">
-            <label :class="`${clsBlockName}-form-item-label`">投递时间</label>
-            <div :class="`${clsBlockName}-form-item-content`">
-              <bp-date-picker
-                placeholder="选择投递时间"
-                :style="{ width: '260px' }"
-                show-time
-                value-format="YYYY-MM-DD HH:mm:ss"></bp-date-picker>
-            </div>
-          </div>
-          <div :class="`${clsBlockName}-form-item`">
+          <bp-row style="width: 100%">
+            <bp-col :span="24">
+              <div :class="`${clsBlockName}-form-item no-line`">
+                <label :class="`${clsBlockName}-form-item-label`">寄给</label>
+                <div :class="`${clsBlockName}-form-item-content`">
+                  <radio-bar v-model="current" size="small" :option-list="radioBarList"></radio-bar>
+                </div>
+              </div>
+              <div :class="`${clsBlockName}-form-item no-line justify-end!`">
+                <label :class="`${clsBlockName}-form-item-label`">是否公开</label>
+                <div :class="`${clsBlockName}-form-item-content`" style="flex: none">
+                  <bp-switch v-model="isPublic"></bp-switch>
+                </div>
+              </div>
+            </bp-col>
+
+            <bp-col :span="24" v-if="current === 'email'">
+              <div :class="`${clsBlockName}-form-item`">
+                <label :class="`${clsBlockName}-form-item-label`">收件地址：</label>
+                <div :class="`${clsBlockName}-form-item-content`">
+                  <bp-input clearable> </bp-input>
+                </div>
+              </div>
+              <div :class="`${clsBlockName}-form-item`">
+                <label :class="`${clsBlockName}-form-item-label`">收件人称呼：</label>
+                <div :class="`${clsBlockName}-form-item-content`">
+                  <bp-input clearable> </bp-input>
+                </div>
+              </div>
+            </bp-col>
+
+            <bp-col :span="24">
+              <div :class="`${clsBlockName}-form-item`">
+                <label :class="`${clsBlockName}-form-item-label`">主题：</label>
+                <div :class="`${clsBlockName}-form-item-content`">
+                  <bp-input clearable :maxlength="25" show-limit> </bp-input>
+                </div>
+              </div>
+            </bp-col>
+
+            <bp-col :span="8">
+              <div :class="`${clsBlockName}-form-item`">
+                <label :class="`${clsBlockName}-form-item-label`">投递时间：</label>
+                <div :class="`${clsBlockName}-form-item-content`">
+                  <bp-date-picker :style="{ width: '100%' }" show-time value-format="YYYY-MM-DD HH:mm:ss"></bp-date-picker>
+                </div>
+              </div>
+            </bp-col>
+          </bp-row>
+          <!-- <div :class="`${clsBlockName}-form-item`">
             <label :class="`${clsBlockName}-form-item-label`">是否公开</label>
             <div :class="`${clsBlockName}-form-item-content`">
               <bp-switch v-model="isPublic"></bp-switch>
@@ -52,9 +67,9 @@
             <div :class="`${clsBlockName}-form-item-content`">
               <public-type-selector v-model="public_type" />
             </div>
-          </div>
+          </div> -->
 
-          <div :class="`${clsBlockName}-form-item column-layout`">
+          <div :class="`${clsBlockName}-form-item no-line column-layout`">
             <yuque-editor ref="editorRef"></yuque-editor>
             <span class="word-count">字数 {{ editorRef?.wordCount }}</span>
           </div>
