11772baa6a60af996ae3b10d8ab3cb6bbd404ad3
Sam <llh@birdpaper.com>
2025-05-20 17:08:18 +0800

    feat: 优化信件项组件，使用计算属性处理头像和昵称显示；简化属性定义

--- app/src/views/write/index.vue
+++ app/src/views/write/index.vue
@@ -53,40 +53,45 @@
             </bp-col>
           </bp-row>
 
-          <bp-row style="width: 100%" :gutter="16" v-if="form.recipient_type === 'email'">
-            <bp-col :xs="24" :sm="16" :md="16" :lg="16" :xl="16">
-              <div :class="`${clsBlockName}-form-item`">
-                <label :class="`${clsBlockName}-form-item-label`">{{ formField.recipient_email }}</label>
-                <div :class="`${clsBlockName}-form-item-content`">
-                  <bp-input v-model="form.recipient_email" clearable> </bp-input>
-                </div>
-              </div>
-            </bp-col>
-            <bp-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">
-              <div :class="`${clsBlockName}-form-item`">
-                <label :class="`${clsBlockName}-form-item-label`">{{ formField.recipient_name }}</label>
-                <div :class="`${clsBlockName}-form-item-content`">
-                  <bp-input v-model="form.recipient_name" clearable> </bp-input>
-                </div>
-              </div>
-            </bp-col>
-          </bp-row>
-          <bp-row style="width: 100%" v-if="form.type === 'public'">
-            <bp-col :span="24">
-              <div :class="`${clsBlockName}-form-item no-line public-config`" style="align-items: flex-start">
-                <label :class="`${clsBlockName}-form-item-label`">{{ formField.public_type }}</label>
-                <div :class="`${clsBlockName}-form-item-content`">
-                  <public-type-selector v-model="form.public_type" v-bind="publicTypeSelectorAttr" style="margin-top: 8px" />
-
-                  <!-- Mobile -->
-                  <div class="public-type-selector-mobile">
-                    <span>{{ publicTypeList.find((v) => v.type === form.public_type).name }}</span>
-                    <bp-button type="text" size="small" @click="showPublicConfig">{{ t("write.editor.public_config_text") }}</bp-button>
+          <transition mode="out-in" name="fade">
+            <bp-row style="width: 100%" :gutter="16" v-if="form.recipient_type === 'email'">
+              <bp-col :xs="24" :sm="16" :md="16" :lg="16" :xl="16">
+                <div :class="`${clsBlockName}-form-item`">
+                  <label :class="`${clsBlockName}-form-item-label`">{{ formField.recipient_email }}</label>
+                  <div :class="`${clsBlockName}-form-item-content`">
+                    <bp-input v-model="form.recipient_email" clearable> </bp-input>
                   </div>
                 </div>
-              </div>
-            </bp-col>
-          </bp-row>
+              </bp-col>
+              <bp-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8">
+                <div :class="`${clsBlockName}-form-item`">
+                  <label :class="`${clsBlockName}-form-item-label`">{{ formField.recipient_name }}</label>
+                  <div :class="`${clsBlockName}-form-item-content`">
+                    <bp-input v-model="form.recipient_name" clearable> </bp-input>
+                  </div>
+                </div>
+              </bp-col>
+            </bp-row>
+          </transition>
+
+          <transition mode="out-in" name="fade">
+            <bp-row style="width: 100%" v-if="form.type === 'public'">
+              <bp-col :span="24">
+                <div :class="`${clsBlockName}-form-item no-line public-config`" style="align-items: flex-start">
+                  <label :class="`${clsBlockName}-form-item-label`">{{ formField.public_type }}</label>
+                  <div :class="`${clsBlockName}-form-item-content`">
+                    <public-type-selector v-model="form.public_type" v-bind="publicTypeSelectorAttr" style="margin-top: 8px" />
+  
+                    <!-- Mobile -->
+                    <div class="public-type-selector-mobile">
+                      <span>{{ publicTypeList.find((v) => v.type === form.public_type).name }}</span>
+                      <bp-button type="text" size="small" @click="showPublicConfig">{{ t("write.editor.public_config_text") }}</bp-button>
+                    </div>
+                  </div>
+                </div>
+              </bp-col>
+            </bp-row>
+          </transition>
 
           <div :class="`${clsBlockName}-form-item no-line column-layout`">
             <yuque-editor ref="editorRef" :placeholder="t('write.editor.placeholder')" :paragraph-tip="''"></yuque-editor>
--- fpo-ui/components/letter-item/src/letter-item.vue
+++ fpo-ui/components/letter-item/src/letter-item.vue
@@ -2,16 +2,16 @@
   <div :class="[clsBlockName, `${clsBlockName}-size-${size}`]">
     <div class="header">
       <div class="sender-info">
-        <bp-avatar size="small" :image-url="avatar"></bp-avatar>
+        <bp-avatar size="small" :image-url="_avatar"></bp-avatar>
         <div class="sender-name">
-          <span class="sender-name-inner">{{ nickName || "--" }}</span>
+          <span class="sender-name-inner">{{ _niclkName || "--" }}</span>
         </div>
       </div>
       <div v-if="size !== 'small'" class="time-ago">{{ createdAt || "--" }}</div>
     </div>
 
     <div class="body">
-      <div class="letter-to">寄给{{ deliveryTime || "--" }}后的{{ isSelf ? "自己" : recipientName || "--" }}</div>
+      <div class="letter-to">寄给{{ deliveryTime || "--" }}后的{{ isSelf ? "自己" : _recipientName || "--" }}</div>
       <div v-if="size !== 'small'" class="letter-title">{{ title || "--" }}</div>
     </div>
 
@@ -34,53 +34,45 @@ defineOptions({ name: "LetterItem" });
 const clsBlockName = "letter-item";
 
 const props = defineProps({
-  size: {
-    type: String as PropType<"small" | "normal">,
-    default: "normal",
-  },
-  mode: {
-    type: String as PropType<"full" | "privary" | "anonymity">,
-    default: "full",
-  },
-  type: {
-    type: String as PropType<"self" | "email">,
-    default: "self",
-  },
-  no: {
-    type: String,
-  },
-  title: {
-    type: String,
-  },
-  avatar: {
-    type: String,
-  },
-  nickName: {
-    type: String,
-  },
-  deliveryTime: {
-    type: String,
-    default: "",
-  },
-  recipientName: {
-    type: String,
-    default: "",
-  },
-  createdAt: {
-    type: String,
-    default: "",
-  },
-  likes: {
-    type: Number,
-    default: 0,
-  },
-  comments: {
-    type: Number,
-    default: 0,
-  },
+  size: { type: String as PropType<"small" | "normal">, default: "normal" },
+  mode: { type: String as PropType<"full" | "privary" | "anonymity">, default: "full" },
+  type: { type: String as PropType<"self" | "email">, default: "self" },
+  no: { type: String },
+  title: { type: String },
+  avatar: { type: String },
+  nickName: { type: String },
+  deliveryTime: { type: String },
+  recipientName: { type: String },
+  createdAt: { type: String },
+  likes: { type: Number },
+  comments: { type: Number },
 });
 
-const isSelf = computed(() => {
-  return props.type === "self";
+const isSelf = computed(() => props.type === "self");
+const _avatar = computed(() => {
+  if (props.mode === "anonymity") {
+    return "";
+  }
+  return props.avatar;
+});
+
+const _niclkName = computed(() => {
+  if (props.mode === "anonymity") {
+    return "匿名";
+  }
+  if (props.mode === "privary") {
+    return props.nickName.slice(0, 1) + "**";
+  }
+  return props.nickName;
+});
+
+const _recipientName = computed(() => {
+  if (props.mode === "anonymity") {
+    return "***";
+  }
+  if (props.mode === "privary") {
+    return props.recipientName.slice(0, 1) + "**";
+  }
+  return props.recipientName;
 });
 </script>
