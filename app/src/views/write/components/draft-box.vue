aa38d777a6729f5920c4535cc337994ba79348d4
Sam <llh@birdpaper.com>
2025-05-26 23:29:05 +0800

    feat: 添加草稿箱样式和组件逻辑；更新草稿箱模态框内容以显示草稿信息

--- app/src/views/write/components/draft-box.vue
+++ app/src/views/write/components/draft-box.vue
@@ -1,6 +1,20 @@
 <template>
-  <bp-modal v-model="show" title="草稿箱" :show-border="false">
-    <bp-empty />
+  <bp-modal v-model="show" title="草稿箱" :show-border="false" width="720px">
+    <div :class="`${clsBlockName}-body`">
+      <!-- <bp-empty /> -->
+      <div v-for="v in 10" :class="`${clsBlockName}-item`">
+        <div class="left">
+          <div class="title">草稿标题草题草稿标题草稿标题</div>
+          <div class="remark">
+            <p>3 天前</p>
+            <p>256 字</p>
+          </div>
+        </div>
+        <div class="right">
+          <bp-button type="text" size="small">删除</bp-button>
+        </div>
+      </div>
+    </div>
   </bp-modal>
 </template>
 
@@ -8,8 +22,9 @@
 import { ref } from "vue";
 
 defineOptions({ name: "DraftBox" });
-const show = ref(false);
+const clsBlockName = "draft-box";
 
+const show = ref(false);
 const open = () => {
   show.value = true;
 };
