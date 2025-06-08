9ab397bbdb789dc13711d300c80b7bb22f97f56e
sam <llh@birdpaper.com>
2025-06-08 08:56:52 +0800

    feat: 更新20250819活动页面样式；添加样式类并优化模板结构，增强可读性

--- app/src/views/activity/20250819/index.vue
+++ app/src/views/activity/20250819/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>20250819</div>
+  <div :class="clsBlockName">
+    <p>20250819</p>
+  </div>
 </template>
+
+<script setup lang="ts">
+const clsBlockName = "activity-20250819";
+</script>
