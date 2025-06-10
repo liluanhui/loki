1f50b7ffae5b4e0a4d6486170fb2d6866b6effd3
Sam <llh@birdpaper.com>
2025-06-10 17:27:43 +0800

    feat: 更新活动页面样式，添加头部和横幅；优化logo组件以支持动态主题和图像

--- app/src/views/activity/20250819/index.vue
+++ app/src/views/activity/20250819/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div :class="clsBlockName">
-    <p>20250819</p>
+    <div :class="`${clsBlockName}-header`">
+      <the-logo theme="light" append="@2x" />
+    </div>
+
+    <div :class="`${clsBlockName}-banner`">
+      <img src="https://fpo-1251999712.cos.ap-guangzhou.myqcloud.com/activity/20250819/letter-banner@1.5x.png?imageMogr2/thumbnail/!80p" alt="" />
+    </div>
+    <p v-for="(v, k) in 100">文本内容{{ k }}</p>
   </div>
 </template>
 
--- fpo-ui/components/the-logo/src/the-logo.vue
+++ fpo-ui/components/the-logo/src/the-logo.vue
@@ -3,13 +3,19 @@
     <img
       v-if="onlyIcon"
       :class="`${clsBlockName}-img`"
-      src="https://fpo-1251999712.cos.ap-guangzhou.myqcloud.com/logo/logo-icon.png"
+      :src="`https://fpo-1251999712.cos.ap-guangzhou.myqcloud.com/logo/logo-icon${append}.png?imageMogr2/thumbnail/!80p`"
       alt="FPO" />
-    <img v-else :class="`${clsBlockName}-img`" src="https://fpo-1251999712.cos.ap-guangzhou.myqcloud.com/logo/logo-text.png" alt="FPO" />
+    <img
+      v-else
+      :class="`${clsBlockName}-img`"
+      :src="`https://fpo-1251999712.cos.ap-guangzhou.myqcloud.com/logo/${logoTextFileName}?imageMogr2/thumbnail/!80p`"
+      alt="FPO" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed, PropType } from "vue";
+
 defineOptions({ name: "TheLogo" });
 const clsBlockName = "the-logo";
 
@@ -18,5 +24,17 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  theme: {
+    type: String as PropType<"light" | "brand">,
+    default: "brand",
+  },
+  append: {
+    type: String,
+    default: "",
+  },
+});
+
+const logoTextFileName = computed(() => {
+  return props.theme === "brand" ? `logo-text${props.append}.png` : `logo-text-light${props.append}.png`;
 });
 </script>
